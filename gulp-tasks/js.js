4059b7fba7f97621cc904a37504f7e61dbaa1635
Iván Meléndez <argordmel@yahoo.es>
2016-02-18 11:09:29 -0500

    Gulp task

--- gulp-tasks/js.js
+++ gulp-tasks/js.js
@@ -15,7 +15,30 @@ module.exports = function (gulp, plugins, config) {
     'use strict';
 
     return function () {
-        
+        gulp.src([config.js])
+        .pipe(plugins.requirejs({
+            ptimize: 'none',
+            wrapShim: true,
+            baseUrl: './',
+            mainConfigFile: 'src/js/main.js',
+            name: 'app',
+            out: 'js/app.js'
+        }))
+        .pipe(plugins.amdclean.gulp({
+            removeAllRequires: true
+        }))
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest(config.dist+'/js/'));
+
+        /*
+        gulp.src([config.js])
+        .pipe(plugins.concat('main.js'))
+        .pipe(plugins.rename({suffix: '.min'}))
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest(config.dist+'/js/'));
+        */
+
+
     };
 
 };
